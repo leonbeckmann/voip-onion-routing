a65b47e14ae87bda483ff035e23a1688d6cea293
Leon <leon.beckmann@tum.de>
2021-05-06 15:33:58 +0200

    Use ToSocketAddr trait instead of parsing the api_addresses via regex. Add rps api_address parsing

--- onion_lib/src/config_parser/mod.rs
+++ onion_lib/src/config_parser/mod.rs
@@ -1,6 +1,5 @@
 
 extern crate ini;
-extern crate regex;
 
 use std::fmt::Formatter;
 use std::path::Path;
@@ -8,16 +7,15 @@ use ini::Ini;
 use openssl::rsa::Rsa;
 use std::fs;
 use openssl::pkey::Public;
-use std::net::{SocketAddrV6, SocketAddrV4};
-use crate::CustomSocketAddr;
-use regex::Regex;
+use std::net::{ToSocketAddrs, SocketAddr};
 
 pub struct OnionConfiguration {
     p2p_port: u16,
     p2p_hostname: String,
     host_key: Rsa<Public>,
     hop_count: u8,
-    api_address: CustomSocketAddr,
+    onion_api_address: SocketAddr,
+    rps_api_address: SocketAddr,
 }
 
 impl OnionConfiguration {
@@ -35,6 +33,13 @@ impl OnionConfiguration {
         let onion_sec = match config.section(Some("onion")) {
             None => {
                 return Err(ParsingError::from_str("Missing section: 'onion'"))
+            }
+            Some(section) => section
+        };
+
+        let rps_sec = match config.section(Some("rps")) {
+            None => {
+                return Err(ParsingError::from_str("Missing section: 'rps'"))
             }
             Some(section) => section
         };
@@ -92,37 +97,55 @@ impl OnionConfiguration {
                 return Err(ParsingError::from_str("Missing component: 'hop_count'"));
             }
             Some(count) => match count.parse::<u8>() {
-                Ok(count) => count,
+                Ok(count) => {
+                    // must be at least two
+                    if count < 2 {
+                        return Err(ParsingError::from_str("hop_count must be at least 2"))
+                    }
+                    count
+                }
                 Err(_) => {
                     return Err(ParsingError::from_str("Cannot parse 'hop_count' to u8"))
                 }
             }
         };
 
-        // parse api_address
-        let hostname_regex = Regex::new(r"^(\S+):(\b\d{1,5}\b)$").unwrap();
-        let api_address = match onion_sec.get("api_address") {
+        // parse onion's api_address
+        let onion_api_address = match onion_sec.get("api_address") {
             None => {
-                return Err(ParsingError::from_str("Missing component: 'api_address'"));
+                return Err(ParsingError::from_str("Missing component: onion's 'api_address'"));
             }
-            Some(address) => {
-                if let Ok(ipv6) = address.parse::<SocketAddrV6>() {
-                    CustomSocketAddr::V6(ipv6)
-                } else if let Ok(ipv4) = address.parse::<SocketAddrV4>() {
-                    CustomSocketAddr::V4(ipv4)
-                } else if let Some(cap) = hostname_regex.captures(address) {
+            Some(address) => match address.to_socket_addrs() {
+                Ok(mut iter) => match iter.next() {
+                    None => {
+                        return Err(ParsingError::from_str("Cannot parse onion's api_address"));
+                    }
+                    Some(addr) => addr
+                }
+                Err(e) => {
+                    return Err(ParsingError::from_string(
+                        format!("Cannot parse onion's api_address: {}", e.to_string()))
+                    )
+                }
+            }
+        };
 
-                    let port = match cap.get(2).unwrap().as_str().parse::<u16>() {
-                        Ok(port) => port,
-                        Err(_) => {
-                            return Err(ParsingError::from_str("Cannot parse api_address port to u16"));
-                        }
-                    };
-
-                    CustomSocketAddr::Hostname(cap.get(1).unwrap().as_str().to_string(), port)
-
-                } else {
-                    return Err(ParsingError::from_str("Cannot match valid api_connection address format"));
+        // parse RPS's api_address
+        let rps_api_address = match rps_sec.get("api_address") {
+            None => {
+                return Err(ParsingError::from_str("Missing component: rps's 'api_address'"));
+            }
+            Some(address) => match address.to_socket_addrs() {
+                Ok(mut iter) => match iter.next() {
+                    None => {
+                        return Err(ParsingError::from_str("Cannot parse rps's api_address"));
+                    }
+                    Some(addr) => addr
+                }
+                Err(e) => {
+                    return Err(ParsingError::from_string(
+                        format!("Cannot parse rps's api_address: {}", e.to_string()))
+                    )
                 }
             }
         };
@@ -132,7 +155,8 @@ impl OnionConfiguration {
             p2p_hostname,
             host_key,
             hop_count,
-            api_address
+            onion_api_address,
+            rps_api_address,
         })
     }
 }
@@ -168,13 +192,51 @@ impl std::fmt::Display for ParsingError {
 mod tests {
 
     extern crate tempdir;
-
     use super::ini::Ini;
     use openssl::rsa::Rsa;
     use tempdir::TempDir;
     use crate::config_parser::OnionConfiguration;
     use std::fs::File;
     use std::io::Write;
+    use std::path::Path;
+
+    fn create_config_file<P: AsRef<Path>>(
+        onion: bool,
+        rps: bool,
+        p2p_port: Option<&str>,
+        p2p_hostname: Option<&str>,
+        hop_count: Option<&str>,
+        onion_api_addr: Option<&str>,
+        rps_api_addr: Option<&str>,
+        hostkey: Option<&str>,
+        file_path: P
+    ) {
+        let mut config = Ini::new();
+        if onion {
+            if let Some(port) = p2p_port {
+                config.with_section(Some("onion")).set("p2p_port", port);
+            }
+            if let Some(hostname) = p2p_hostname {
+                config.with_section(Some("onion")).set("p2p_hostname", hostname);
+            }
+            if let Some(hop_count) = hop_count {
+                config.with_section(Some("onion")).set("hop_count", hop_count);
+            }
+            if let Some(key) = hostkey {
+                config.with_section(Some("onion")).set("hostkey", key);
+            }
+            if let Some(api_addr) = onion_api_addr {
+                config.with_section(Some("onion")).set("api_address", api_addr);
+            }
+        }
+        if rps {
+            if let Some(api_addr) = rps_api_addr {
+                config.with_section(Some("rps")).set("api_address", api_addr);
+            }
+        }
+
+        config.write_to_file(file_path).unwrap();
+    }
 
     #[test]
     fn unit_config_parser() {
@@ -190,14 +252,14 @@ mod tests {
         let config_missing_port = dir.path().join("missing-port.config");
         let config_invalid_port = dir.path().join("invalid-port.config");
         let config_missing_hostname = dir.path().join("missing-hostname.config");
-        let config_additional_information = dir.path().join("add-info.config");
         let config_missing_section = dir.path().join("missing-section.config");
         let config_missing_hostkey = dir.path().join("missing-hostkey.config");
         let config_der_hostkey = dir.path().join("der-hostkey.config");
         let config_invalid_hostkey_path = dir.path().join("invalid_hostkey.config");
         let config_missing_hop_count = dir.path().join("missing_hops.config");
         let config_invalid_hop_count = dir.path().join("invalid_hops.config");
-        let config_missing_api_address = dir.path().join("missing_api_address.config");
+        let config_missing_onion_api_address = dir.path().join("missing_o_api_address.config");
+        let config_missing_rps_api_address = dir.path().join("missing_r_api_address.config");
         let config_invalid_api_address = dir.path().join("invalid_api_address.config");
         let config_api_address_v6 = dir.path().join("api_address_v6.config");
 
@@ -215,131 +277,76 @@ mod tests {
         rsa_der.sync_all().unwrap();
 
         // create config files
-        let mut config = Ini::new();
-        config.with_section(Some("onion"))
-            .set("p2p_port", "1234")
-            .set("p2p_hostname", "localhost")
-            .set("hop_count", "2")
-            .set("api_address", "127.0.0.1:12345")
-            .set("hostkey", host_key_file.to_str().unwrap());
-        config.write_to_file(&valid_config).unwrap();
+        create_config_file(true, true, Some("1234"), Some("localhost"),
+                           Some("2"), Some("127.0.0.1:1234"), Some("127.0.0.1:1235"),
+                           Some(host_key_file.to_str().unwrap()), &valid_config);
 
-        let config = Ini::new();
-        config.write_to_file(&config_missing_section).unwrap();
+        create_config_file(false, false, None, None, None,
+                           None, None, None, &config_missing_section);
 
-        let mut config = Ini::new();
-        config.with_section(Some("onion"))
-            .set("p2p_hostname", "localhost")
-            .set("hop_count", "2")
-            .set("api_address", "127.0.0.1:12345")
-            .set("hostkey", host_key_file.to_str().unwrap());
-        config.write_to_file(&config_missing_port).unwrap();
+        create_config_file(true, true, None, Some("localhost"),
+                           Some("2"), Some("localhost:1234"), Some("localhost:1235"),
+                           Some(host_key_file.to_str().unwrap()), &config_missing_port);
 
-        let mut config = Ini::new();
-        config.with_section(Some("onion"))
-            .set("p2p_port", "invalid")
-            .set("p2p_hostname", "localhost")
-            .set("hop_count", "2")
-            .set("api_address", "127.0.0.1:12345")
-            .set("hostkey", host_key_file.to_str().unwrap());
-        config.write_to_file(&config_invalid_port).unwrap();
+        create_config_file(true, true, Some("2x"), Some("localhost"),
+                           Some("2"), Some("localhost:1234"), Some("localhost:1235"),
+                           Some(host_key_file.to_str().unwrap()), &config_invalid_port);
 
-        let mut config = Ini::new();
-        config.with_section(Some("onion"))
-            .set("p2p_port", "1234")
-            .set("hop_count", "2")
-            .set("api_address", "127.0.0.1:12345")
-            .set("hostkey", host_key_file.to_str().unwrap());
-        config.write_to_file(&config_missing_hostname).unwrap();
+        create_config_file(true, true, Some("1234"), None,
+                           Some("2"), Some("localhost:1234"), Some("localhost:1235"),
+                           Some(host_key_file.to_str().unwrap()), &config_missing_hostname);
 
-        let mut config = Ini::new();
-        config.with_section(Some("onion"))
-            .set("p2p_port", "1234")
-            .set("p2p_hostname", "localhost")
-            .set("hop_count", "2")
-            .set("api_address", "127.0.0.1:12345");
-        config.write_to_file(&config_missing_hostkey).unwrap();
+        create_config_file(true, true, Some("1234"), Some("localhost"),
+                           Some("2"), Some("localhost:1234"), Some("localhost:1235"),
+                           None, &config_missing_hostkey);
 
-        let mut config = Ini::new();
-        config.with_section(Some("onion"))
-            .set("p2p_port", "1234")
-            .set("p2p_hostname", "localhost")
-            .set("hop_count", "2")
-            .set("api_address", "127.0.0.1:12345")
-            .set("hostkey", invalid_host_key_file.to_str().unwrap());
-        config.write_to_file(&config_der_hostkey).unwrap();
+        create_config_file(true, true, Some("1234"), Some("localhost"),
+                           Some("2"), Some("localhost:1234"), Some("localhost:1235"),
+                           Some(invalid_host_key_file.to_str().unwrap()), &config_der_hostkey);
 
-        let mut config = Ini::new();
-        config.with_section(Some("onion"))
-            .set("p2p_port", "1234")
-            .set("p2p_hostname", "localhost")
-            .set("hop_count", "2")
-            .set("api_address", "127.0.0.1:12345")
-            .set("hostkey", invalid_host_key_path.to_str().unwrap());
-        config.write_to_file(&config_invalid_hostkey_path).unwrap();
+        create_config_file(true, true, Some("1234"), Some("localhost"),
+                           Some("2"), Some("localhost:1234"), Some("localhost:1235"),
+                           Some(invalid_host_key_path.to_str().unwrap()), &config_invalid_hostkey_path);
 
-        let mut config = Ini::new();
-        config.with_section(Some("onion"))
-            .set("p2p_port", "1234")
-            .set("p2p_hostname", "localhost")
-            .set("hop_count", "2")
-            .set("api_address", "127.0.0.1:12345")
-            .set("dummy_key", "some_value")
-            .set("hostkey", host_key_file.to_str().unwrap());
-        config.with_section(Some("another_section"))
-            .set("random_data", "value");
-        config.write_to_file(&config_additional_information).unwrap();
+        create_config_file(true, true, Some("1234"), Some("localhost"),
+                           Some("2"), Some("localhost:1234"), Some("localhost:1235"),
+                           Some(host_key_file.to_str().unwrap()), &valid_config);
 
-        let mut config = Ini::new();
-        config.with_section(Some("onion"))
-            .set("p2p_port", "1234")
-            .set("p2p_hostname", "localhost")
-            .set("api_address", "127.0.0.1:12345")
-            .set("hostkey", host_key_file.to_str().unwrap());
-        config.write_to_file(&config_missing_hop_count).unwrap();
+        create_config_file(true, true, Some("1234"), Some("localhost"),
+                           None, Some("localhost:1234"), Some("localhost:1235"),
+                           Some(host_key_file.to_str().unwrap()), &config_missing_hop_count);
 
-        let mut config = Ini::new();
-        config.with_section(Some("onion"))
-            .set("p2p_port", "1234")
-            .set("p2p_hostname", "localhost")
-            .set("hop_count", "256")
-            .set("api_address", "127.0.0.1:12345")
-            .set("hostkey", host_key_file.to_str().unwrap());
-        config.write_to_file(&config_invalid_hop_count).unwrap();
+        create_config_file(true, true, Some("1234"), Some("localhost"),
+                           Some("1"), Some("localhost:1234"), Some("localhost:1235"),
+                           Some(host_key_file.to_str().unwrap()), &config_invalid_hop_count);
 
-        let mut config = Ini::new();
-        config.with_section(Some("onion"))
-            .set("p2p_port", "1234")
-            .set("p2p_hostname", "localhost")
-            .set("hop_count", "2")
-            .set("hostkey", host_key_file.to_str().unwrap());
-        config.write_to_file(&config_missing_api_address).unwrap();
+        create_config_file(true, true, Some("1234"), Some("localhost"),
+                           Some("2"), Some("localhost:1234"), Some("localhost:1235"),
+                           Some(host_key_file.to_str().unwrap()), &valid_config);
 
-        let mut config = Ini::new();
-        config.with_section(Some("onion"))
-            .set("p2p_port", "1234")
-            .set("p2p_hostname", "localhost")
-            .set("hop_count", "2")
-            .set("api_address", "127.0.0.1:123456")
-            .set("hostkey", host_key_file.to_str().unwrap());
-        config.write_to_file(&config_invalid_api_address).unwrap();
+        create_config_file(true, true, Some("1234"), Some("localhost"),
+                           Some("2"), None, Some("localhost:1235"),
+                           Some(host_key_file.to_str().unwrap()), &config_missing_onion_api_address);
 
-        let mut config = Ini::new();
-        config.with_section(Some("onion"))
-            .set("p2p_port", "1234")
-            .set("p2p_hostname", "localhost")
-            .set("hop_count", "2")
-            .set("api_address", "[::1]:12345")
-            .set("hostkey", host_key_file.to_str().unwrap());
-        config.write_to_file(&config_api_address_v6).unwrap();
+        create_config_file(true, true, Some("1234"), Some("localhost"),
+                           Some("2"), Some("localhost:1234"), None,
+                           Some(host_key_file.to_str().unwrap()), &config_missing_rps_api_address);
+
+        create_config_file(true, true, Some("1234"), Some("localhost"),
+                           Some("2"), Some("127.0.0.1:123400"), Some("localhost:1235"),
+                           Some(host_key_file.to_str().unwrap()), &config_invalid_api_address);
+
+        create_config_file(true, true, Some("1234"), Some("localhost"),
+                           Some("2"), Some("[::1]:1234"), Some("localhost:1235"),
+                           Some(host_key_file.to_str().unwrap()), &config_api_address_v6);
 
         // parse configurations
         let config = OnionConfiguration::parse_from_file(valid_config).unwrap();
         assert_eq!(config.p2p_port, 1234);
         assert_eq!(config.p2p_hostname, "localhost");
         assert_eq!(config.hop_count, 2);
-        assert_eq!(config.api_address.port(), 12345);
-        assert!(config.api_address.is_ipv4());
+        assert_eq!(config.onion_api_address.port(), 1234);
+        assert_eq!(config.rps_api_address.port(), 1235);
         assert_eq!(config.host_key.public_key_to_pem().unwrap(), pub_pem);
 
         assert!(OnionConfiguration::parse_from_file(config_missing_section).is_err());
@@ -349,17 +356,16 @@ mod tests {
         assert!(OnionConfiguration::parse_from_file(config_invalid_hostkey_path).is_err());
         assert!(OnionConfiguration::parse_from_file(config_missing_hostkey).is_err());
         assert!(OnionConfiguration::parse_from_file(config_der_hostkey).is_err());
-        assert!(OnionConfiguration::parse_from_file(config_additional_information).is_ok());
         assert!(OnionConfiguration::parse_from_file(config_missing_hop_count).is_err());
         assert!(OnionConfiguration::parse_from_file(config_invalid_hop_count).is_err());
-        assert!(OnionConfiguration::parse_from_file(config_missing_api_address).is_err());
+        assert!(OnionConfiguration::parse_from_file(config_missing_onion_api_address).is_err());
+        assert!(OnionConfiguration::parse_from_file(config_missing_rps_api_address).is_err());
         assert!(OnionConfiguration::parse_from_file(config_invalid_api_address).is_err());
 
         let config =
             OnionConfiguration::parse_from_file(config_api_address_v6).unwrap();
-        assert_eq!(config.api_address.port(), 12345);
-        assert!(config.api_address.is_ipv6());
-        assert_eq!(config.host_key.public_key_to_pem().unwrap(), pub_pem);
+        assert_eq!(config.onion_api_address.port(), 1234);
+        assert!(config.onion_api_address.is_ipv6());
 
 
         dir.close().unwrap();
--- onion_lib/src/lib.rs
+++ onion_lib/src/lib.rs
@@ -1,46 +1,7 @@
 
-use std::net::{SocketAddrV4, SocketAddrV6};
-
 mod config_parser;
 mod api_protocol;
 mod p2p_protocol;
-
-pub(crate) enum CustomSocketAddr {
-    V4(SocketAddrV4),
-    V6(SocketAddrV6),
-    Hostname(String, u16)
-}
-
-impl CustomSocketAddr {
-    pub fn is_ipv4(&self) -> bool {
-        match self {
-            CustomSocketAddr::V4(_) => true,
-            _ => false
-        }
-    }
-
-    pub fn is_ipv6(&self) -> bool {
-        match self {
-            CustomSocketAddr::V6(_) => true,
-            _ => false
-        }
-    }
-
-    pub fn is_hostname(&self) -> bool {
-        match self {
-            CustomSocketAddr::Hostname(_,_) => true,
-            _ => false
-        }
-    }
-
-    pub fn port(&self) -> u16 {
-        match self {
-            CustomSocketAddr::V4(ip) => ip.port(),
-            CustomSocketAddr::V6(ip) => ip.port(),
-            CustomSocketAddr::Hostname(_, port) => *port
-        }
-    }
-}
 
 
 #[cfg(test)]
