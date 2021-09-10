1963ca79c31c36280bf32f0f590342c05b5e4c1d
Florian Freund <florian88freund@gmail.com>
2021-09-10 20:25:04 +0200

    Fix tests

--- onion_lib/src/lib.rs
+++ onion_lib/src/lib.rs
@@ -93,14 +93,21 @@ async fn run_peer_async<P: AsRef<Path> + Debug>(config_file: P) {
 
 #[cfg(test)]
 mod tests {
-    use std::{fs::File, io::Write};
+    use std::{fs::File, io::Write, time::Duration};
 
     use ini::Ini;
-    use openssl::rsa::Rsa;
+    use openssl::{
+        asn1::Asn1Time,
+        hash::MessageDigest,
+        nid::Nid,
+        pkey::PKey,
+        rsa::Rsa,
+        x509::{X509Builder, X509Name},
+    };
     use pin_utils::pin_mut;
     use tempdir::TempDir;
 
-    use crate::{run_peer, run_peer_async};
+    use crate::{config_parser::create_config_file, run_peer, run_peer_async};
 
     #[test]
     fn unit_invalid_config() {
@@ -115,108 +122,155 @@ mod tests {
         run_peer(invalid_config_file);
     }
 
-    async fn run_valid_peer(
+    fn run_valid_peer(
         dir: &TempDir,
         p2p_port: &str,
         api_port: &str,
     ) -> tokio::task::JoinHandle<()> {
-        let key_file = dir.path().join("peer.key");
-        let priv_key_file = dir.path().join("peer_priv.key");
-        // create RSA keys
-        let key = Rsa::generate(4096).unwrap();
-        let pub_pem = key.public_key_to_pem().unwrap();
-        let priv_pem = key.private_key_to_pem().unwrap();
+        // create paths for host-key and config files
+        let host_key_file = dir.path().join("hostkey");
+        let invalid_host_key_file = dir.path().join("hostkey-der");
+        let host_key_priv_file = dir.path().join("hostkey_priv");
+        let invalid_host_key_priv_file = dir.path().join("hostkey_priv-der");
+        let empty_host_key_priv_file = dir.path().join("hostkey_priv-empty");
+        let cert_file = dir.path().join("cert-pem");
 
-        let mut rsa_pem = File::create(&key_file).unwrap();
+        let valid_config = dir.path().join("valid.config");
+
+        // create RSA key
+        let key = Rsa::generate(4096).unwrap();
+        let pkey = PKey::from_rsa(key.clone()).unwrap();
+        let pub_pem = key.public_key_to_pem().unwrap();
+        let pub_der = key.public_key_to_der().unwrap();
+        let priv_pem = key.private_key_to_pem().unwrap();
+        let priv_der = key.private_key_to_der().unwrap();
+
+        // create rsa files
+        let mut rsa_pem = File::create(&host_key_file).unwrap();
+        let mut rsa_der = File::create(&invalid_host_key_file).unwrap();
+        let mut rsa_priv_pem = File::create(&host_key_priv_file).unwrap();
+        let mut rsa_priv_der = File::create(&invalid_host_key_priv_file).unwrap();
+        File::create(&empty_host_key_priv_file).unwrap();
         rsa_pem.write_all(pub_pem.as_slice()).unwrap();
         rsa_pem.sync_all().unwrap();
-        let mut rsa_priv_pem = File::create(&priv_key_file).unwrap();
+        rsa_der.write_all(pub_der.as_slice()).unwrap();
+        rsa_der.sync_all().unwrap();
         rsa_priv_pem.write_all(priv_pem.as_slice()).unwrap();
         rsa_priv_pem.sync_all().unwrap();
+        rsa_priv_der.write_all(priv_der.as_slice()).unwrap();
+        rsa_priv_der.sync_all().unwrap();
 
-        let config_file = dir.path().join("config.ini");
+        // create certificate files
+        let mut cert = X509Builder::new().unwrap();
+        let mut name = X509Name::builder().unwrap();
+        name.append_entry_by_nid(Nid::COMMONNAME, "test.com")
+            .unwrap();
+        let name = name.build();
+        cert.set_version(2).unwrap();
+        cert.set_subject_name(&name).unwrap();
+        cert.set_issuer_name(&name).unwrap();
+        cert.set_not_before(&Asn1Time::days_from_now(0).unwrap())
+            .unwrap();
+        cert.set_not_after(&Asn1Time::days_from_now(365).unwrap())
+            .unwrap();
+        cert.set_pubkey(&pkey).unwrap();
+        cert.sign(&pkey, MessageDigest::sha256()).unwrap();
+        let cert = cert.build();
+        let cert_pem = cert.to_pem().unwrap();
+        let mut cert_pem_file = File::create(&cert_file).unwrap();
+        cert_pem_file.write_all(cert_pem.as_slice()).unwrap();
+        cert_pem_file.sync_all().unwrap();
 
-        let mut config = Ini::new();
-        config
-            .with_general_section()
-            .set("hostkey", key_file.to_str().unwrap());
-        config
-            .with_section(Some("onion"))
-            .set("p2p_port", p2p_port)
-            .set("p2p_hostname", "127.0.0.1")
-            .set("hop_count", "2")
-            .set("api_address", format!("127.0.0.1:{}", api_port).as_str())
-            .set("round_time", "5")
-            .set("private_hostkey", priv_key_file.to_str().unwrap())
-            .set("handshake_timeout", "3000");
-        config
-            .with_section(Some("rps"))
-            .set("api_address", "127.0.0.1:8000");
-        config.write_to_file(&config_file).unwrap();
+        create_config_file(
+            true,
+            true,
+            Some(p2p_port),
+            Some("127.0.0.1"),
+            Some("2"),
+            Some(format!("127.0.0.1:{}", api_port).as_str()),
+            Some("127.0.0.1:1235"),
+            Some(host_key_file.to_str().unwrap()),
+            Some(host_key_priv_file.to_str().unwrap()),
+            Some("100"),
+            Some("2000"),
+            Some("1"),
+            Some("1000"),
+            Some(cert_file.to_str().unwrap()),
+            Some(cert_file.to_str().unwrap()),
+            Some("3600"),
+            Some("10000"),
+            Some("10000"),
+            &valid_config,
+        );
 
         // run peer
-        tokio::task::spawn(run_peer_async(config_file))
+        tokio::task::spawn(run_peer_async(valid_config))
     }
 
     #[test]
     fn unit_valid_config() {
+        // create tmp dir that will be automatically dropped afterwards
         let dir = TempDir::new("onion-test").unwrap();
 
         let runtime = tokio::runtime::Runtime::new().unwrap();
         runtime.block_on(async {
-            let peer = run_valid_peer(&dir, "2000", "3001");
+            let peer = run_valid_peer(&dir, "2010", "3011");
             // This should timeout because it's valid
-            // TODO: fix and use unwrap_err() here
-            tokio::time::timeout(std::time::Duration::from_secs(1), peer)
+            tokio::time::timeout(Duration::from_secs(1), peer)
                 .await
-                .unwrap();
+                .unwrap_err();
         });
     }
 
     #[test]
     fn unit_multiple_with_same_p2p_port() {
         let dir = TempDir::new("onion-test").unwrap();
-        let peer1 = run_valid_peer(&dir, "2000", "3000");
-        let peer2 = run_valid_peer(&dir, "2000", "3001");
-        // Pin that it can be passed by reference to timeout and is usable afterwards.
-        pin_mut!(peer1);
-        pin_mut!(peer2);
 
         let runtime = tokio::runtime::Runtime::new().unwrap();
         runtime.block_on(async {
+            let peer1 = run_valid_peer(&dir, "2020", "3020");
+            tokio::time::sleep(Duration::from_millis(100)).await;
+            let peer2 = run_valid_peer(&dir, "2020", "3021");
+        // Pin that it can be passed by reference to timeout and is usable afterwards.
+            // This prevents the abortion of the future which would unbind the port.
+        pin_mut!(peer1);
+        pin_mut!(peer2);
+
             // This should timeout because it's valid
-            // TODO: fix and use unwrap_err() here
             tokio::time::timeout(std::time::Duration::from_secs(1), &mut peer1)
                 .await
-                .unwrap();
+                .unwrap_err();
             // This should not timeout because port is already used
+            // The port bind panics and therefore a JoinError is expected
             tokio::time::timeout(std::time::Duration::from_secs(1), &mut peer2)
                 .await
-                .unwrap();
+                .unwrap().unwrap_err();
         });
     }
 
     #[test]
     fn unit_multiple_with_same_api_port() {
         let dir = TempDir::new("onion-test").unwrap();
-        let peer1 = run_valid_peer(&dir, "2000", "3000");
-        let peer2 = run_valid_peer(&dir, "2001", "3000");
-        // Pin that it can be passed by reference to timeout and is usable afterwards.
-        pin_mut!(peer1);
-        pin_mut!(peer2);
 
         let runtime = tokio::runtime::Runtime::new().unwrap();
         runtime.block_on(async {
+            let peer1 = run_valid_peer(&dir, "2030", "3030");
+            tokio::time::sleep(Duration::from_millis(100)).await;
+            let peer2 = run_valid_peer(&dir, "2031", "3030");
+        // Pin that it can be passed by reference to timeout and is usable afterwards.
+            // This prevents the abortion of the future which would unbind the port.
+        pin_mut!(peer1);
+        pin_mut!(peer2);
+
             // This should timeout because it's valid
-            // TODO: fix and use unwrap_err() here
             tokio::time::timeout(std::time::Duration::from_secs(1), &mut peer1)
                 .await
-                .unwrap();
+                .unwrap_err();
             // This should not timeout because port is already used
+            // The port bind panics and therefore a JoinError is expected
             tokio::time::timeout(std::time::Duration::from_secs(1), &mut peer2)
                 .await
-                .unwrap();
+                .unwrap().unwrap();
         });
     }
 }
-// TODO test running a peer with valid config, shutdown api and p2p protocol
